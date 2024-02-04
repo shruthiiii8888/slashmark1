31ea7063999b76bdfc01dfb37414b05066ec3fae
shruthiiii8888 <115170412+shruthiiii8888@users.noreply.github.com>
2024-02-04 09:55:17 +0530

    Add files via upload

---
+++ slashmark/slash/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeatherForecast from './weather';
+
+const App = () => {
+  return (
+    <div>
+      <WeatherForecast />
+    </div>
+  );
+};
+
+export default App;
